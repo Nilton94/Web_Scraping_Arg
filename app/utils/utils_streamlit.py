80acb509433580be19ac241cefb23edd8d7ce3f4
jose.nilton <jose.nilton@ifood.com.br>
2024-02-07 23:01:18 -0300

    Ajustes widgets

--- app/utils/utils_streamlit.py
+++ app/utils/utils_streamlit.py
@@ -5,6 +5,8 @@ import os
 from utils.lat_long import get_all_states
 from utils.utils_transformations import TiposImoveis, get_columns_intersection
 from folium.plugins import GroupedLayerControl
+import asyncio
+from utils.utils_scraper import ScraperArgenProp, ScraperZonaProp
 
 def get_widgets():
     # WIDGETS
@@ -31,13 +33,38 @@ def get_widgets():
     )
 
     st.sidebar.multiselect(
-        label = 'Colunas da tabela',
-        options = ['id', 'base','tipo_imovel','estado','cidade','bairro', 'endereco', 'url','descricao', 'titulo', 'aluguel_moeda', 'aluguel_valor', 'expensas_moeda', 'expensas_valor', 'valor_total_aluguel',
-                'area_util','ambientes', 'dormitorios', 'banheiros', 'garagens', 'imobiliaria','distancia_hospital_baigorria','distancia_hospital_carrasco','distancia_hospital_ninos','distancia_hospital_provincial',
-                'distancia_unr','latitude','longitude','coordenadas','data','ano','mes','dia'],
-        key = 'colunas_tabela'
+        'Moeda do Aluguel',
+        options = ['USD', '$', 'Sem info'],
+        key = 'aluguel_moeda'
     )
 
+    st.sidebar.slider(
+        'Aluguel',
+        min_value = 0.0,
+        max_value = 999_999_999.0,
+        key = 'aluguel_valor'
+    )
+
+    st.sidebar.multiselect(
+        'Moeda das Expensas',
+        options = ['USD', '$', 'Sem info'],
+        key = 'expensas_moeda'
+    )
+
+    st.sidebar.slider(
+        'Expensas',
+        min_value = 0.0,
+        max_value = 999_999_999.0,
+        key = 'expensas_valor'
+    )
+
+    # st.sidebar.multiselect(
+    #     label = 'Colunas da tabela',
+    #     options = ['id', 'base','tipo_imovel','estado','cidade','bairro', 'endereco', 'url','descricao', 'titulo', 'aluguel_moeda', 'aluguel_valor', 'expensas_moeda', 'expensas_valor', 'valor_total_aluguel',
+    #             'area_util','ambientes', 'dormitorios', 'banheiros', 'garagens', 'imobiliaria','distancia_hospital_baigorria','distancia_hospital_carrasco','distancia_hospital_ninos','distancia_hospital_provincial',
+    #             'distancia_unr','latitude','longitude','coordenadas','data','ano','mes','dia'],
+    #     key = 'colunas_tabela'
+    # )
     
     st.sidebar.slider(
         'Distância para UNR (km)',
@@ -74,19 +101,38 @@ def get_widgets():
         key = 'distancia_baigorria'
     )
 
+    st.sidebar.slider(
+        'Área Útil (m2)',
+        min_value = 0.0,
+        max_value = 999.0,
+        key = 'area_util'
+    )
+
+    st.sidebar.slider(
+        'Banheiros',
+        min_value = 0.0,
+        max_value = 999.0,
+        key = 'banheiros'
+    )
+
+    st.sidebar.slider(
+        'Ambientes',
+        min_value = 0.0,
+        max_value = 999.0,
+        key = 'ambientes'
+    )
+
+    st.sidebar.slider(
+        'Dormitórios',
+        min_value = 0.0,
+        max_value = 999.0,
+        key = 'dormitorios'
+    )
+    
     st.sidebar.button(
         label = 'Atualizar',
         key = 'atualizar'
     )
-
-    # area util
-    # banheiros
-    # ambientes
-    # dormitorios
-    # aluguel moeda
-    # aluguel valor
-    # expensas moeda
-    # expensas valor
 
 def get_map(df: pd.DataFrame, tipo_layer: str = 'bairro'):
     '''
@@ -177,3 +223,78 @@ def get_map(df: pd.DataFrame, tipo_layer: str = 'bairro'):
         ).add_to(m)
 
     return m
+
+def get_dataframe():
+    
+    # DADOS
+    if ('Argenprop' in st.session_state.base_busca) and ('Zonaprop' in st.session_state.base_busca):
+        df_argenprop = asyncio.run(ScraperArgenProp(st.session_state.tipos, st.session_state.locais).get_final_dataframe())
+        df_zonaprop = ScraperZonaProp(_tipo = st.session_state.tipos, _local = st.session_state.locais).get_final_dataframe()
+
+        df_final = (
+            pd.concat(
+                [
+                    df_argenprop.loc[:, get_columns_intersection(df_argenprop, df_zonaprop)],
+                    df_zonaprop.loc[:, get_columns_intersection(df_argenprop, df_zonaprop)]
+                ],
+                ignore_index = True
+            )
+            .pipe(
+                lambda df: df.loc[
+                    (df.distancia_unr.between(0.0, st.session_state.distancia_unr if st.session_state.distancia_unr > 0.0 else 999_999_999.0))
+                    & (df.distancia_hospital_provincial.between(0.0, (st.session_state.distancia_provincial if st.session_state.distancia_provincial > 0.0 else 999_999_999.0)))
+                    & (df.distancia_hospital_ninos.between(0.0, (st.session_state.distancia_ninos if st.session_state.distancia_ninos > 0.0 else 999_999_999.0)))
+                    & (df.distancia_hospital_carrasco.between(0.0, (st.session_state.distancia_carrasco if st.session_state.distancia_carrasco > 0.0 else 999_999_999.0)))
+                    & (df.distancia_hospital_baigorria.between(0.0, (st.session_state.distancia_baigorria if st.session_state.distancia_baigorria > 0.0 else 999_999_999.0)))
+                    & (df.area_util.between(0.0, (st.session_state.area_util if st.session_state.area_util > 0.0 else 999_999_999.0)))
+                    & (df.banheiros.between(0.0, (st.session_state.banheiros if st.session_state.banheiros > 0.0 else 999_999_999.0)))
+                    & (df.ambientes.between(0.0, (st.session_state.ambientes if st.session_state.ambientes > 0.0 else 999_999_999.0)))
+                    & (df.dormitorios.between(0.0, (st.session_state.dormitorios if st.session_state.dormitorios > 0.0 else 999_999_999.0)))
+                    & (df.aluguel_valor.between(0.0, (st.session_state.aluguel_valor if st.session_state.aluguel_valor > 0.0 else 999_999_999.0)))
+                    & (df.expensas_valor.between(0.0, (st.session_state.expensas_valor if st.session_state.expensas_valor > 0.0 else 999_999_999.0)))
+                    # & (df.aluguel_moeda.isin([]))
+                    # 'aluguel_moeda'
+                    # 'aluguel_valor'
+                    # 'expensas_moeda'
+                    # 'expensas_valor'
+                ]
+            )
+            .reset_index(drop = True)
+        )
+
+    elif 'Argenprop' in st.session_state.base_busca:
+        df_argenprop = asyncio.run(ScraperArgenProp(st.session_state.tipos, st.session_state.locais).get_final_dataframe())
+        
+        df_final = (
+            df_argenprop
+            .pipe(
+                lambda df: df.loc[
+                    (df.distancia_unr.between(0.0, st.session_state.distancia_unr if st.session_state.distancia_unr > 0.0 else 60.0))
+                    & (df.distancia_hospital_provincial.between(0.0, (st.session_state.distancia_provincial if st.session_state.distancia_provincial > 0.0 else 60.0)))
+                    & (df.distancia_hospital_ninos.between(0.0, (st.session_state.distancia_ninos if st.session_state.distancia_ninos > 0.0 else 60.0)))
+                    & (df.distancia_hospital_carrasco.between(0.0, (st.session_state.distancia_carrasco if st.session_state.distancia_carrasco > 0.0 else 60.0)))
+                    & (df.distancia_hospital_baigorria.between(0.0, (st.session_state.distancia_baigorria if st.session_state.distancia_baigorria > 0.0 else 60.0)))
+                ]
+            )
+            .reset_index(drop = True)
+        )
+
+    elif 'Zonaprop' in st.session_state.base_busca:
+        df_zonaprop = ScraperZonaProp(_tipo = st.session_state.tipos, _local = st.session_state.locais).get_final_dataframe()
+        
+        df_final = (
+            df_zonaprop
+            .pipe(
+                lambda df: df.loc[
+                    (df.distancia_unr.between(0.0, st.session_state.distancia_unr if st.session_state.distancia_unr > 0.0 else 60.0))
+                    & (df.distancia_hospital_provincial.between(0.0, (st.session_state.distancia_provincial if st.session_state.distancia_provincial > 0.0 else 60.0)))
+                    & (df.distancia_hospital_ninos.between(0.0, (st.session_state.distancia_ninos if st.session_state.distancia_ninos > 0.0 else 60.0)))
+                    & (df.distancia_hospital_carrasco.between(0.0, (st.session_state.distancia_carrasco if st.session_state.distancia_carrasco > 0.0 else 60.0)))
+                    & (df.distancia_hospital_baigorria.between(0.0, (st.session_state.distancia_baigorria if st.session_state.distancia_baigorria > 0.0 else 60.0)))
+                ]
+            )
+            .reset_index(drop = True)
+        )
+
+    else:
+        st.text('Selecione uma ou mais bases!')
